6134bffec20a48f8f7c758e63a93866ce5f4e478
Filip Kittnar <drmorbid@seznam.cz>
2024-05-12 12:02:36 +0200

    Create bare bons basic front end

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: "Practice Tool",
+};
+
+export default function Page() {
+ return (
+  <>
+   <h1>Welcome to Practice Tool</h1>
+  </>
+ );
+}
