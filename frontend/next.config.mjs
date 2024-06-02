0b7061b3eddcbacea033fd064c5a00f7cf8944fa
Filip Kittnar <drmorbid@seznam.cz>
2024-06-02 16:36:43 +0200

    Configure as a static web page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,6 +16,7 @@ const withSerwist = withSerwistInit({
 /** @type {import("next").NextConfig} */
 const nextConfig = {
  reactStrictMode: true,
+ output: "export"
 };
 
 export default withSerwist(nextConfig);
