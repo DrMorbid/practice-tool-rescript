8d5bd3631b7b8a4003f6106c01af4fe2c37b5ade
Filip Kittnar <drmorbid@seznam.cz>
2024-06-02 16:29:55 +0200

    Finalize app icons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,36 +5,36 @@ const APP_NAME = "Practice Tool";
 const APP_DESCRIPTION = "Tool to help you practice what needs to be practiced";
 
 export const metadata: Metadata = {
- applicationName: APP_NAME,
- title: {
-  default: APP_NAME,
-  template: "%s - Practice Tool",
- },
- description: APP_DESCRIPTION,
- manifest: "/manifest.json",
- appleWebApp: {
-  capable: true,
-  statusBarStyle: "default",
-  title: APP_NAME,
- },
- formatDetection: {
-  telephone: false,
- },
- icons: {
-  shortcut: "/favicon.ico",
-  apple: [{ url: "/icons/apple-touch-icon.png", sizes: "180x180" }],
- },
+  applicationName: APP_NAME,
+  title: {
+    default: APP_NAME,
+    template: "%s - Practice Tool",
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_NAME,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/icons/maskable_icon_x192.png", sizes: "192x192" }],
+  },
 };
 
 export const viewport: Viewport = {
- themeColor: "#FFFFFF",
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
- return (
-  <html lang="en" dir="ltr">
-   <head>
-    <style>{`
+  return (
+    <html lang="en" dir="ltr">
+      <head>
+        <style>{`
             html, body, #__next {
               height: 100%;
             }
@@ -45,8 +45,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               text-align: center;
             }
             `}</style>
-   </head>
-   <body>{children}</body>
-  </html>
- );
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
