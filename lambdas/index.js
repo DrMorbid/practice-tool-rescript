7b8580f63d551b871717f6e4395d59c68da5071b
Filip Kittnar <drmorbid@seznam.cz>
2024-10-02 22:06:02 +0200

    Upgrade serverless

---
+++ lambdas/index.js
@@ -0,0 +1,17 @@
+const projectLambdaDelete = require('./src/project/Project_Lambda_Delete.res.js');
+exports.projectLambdaDelete = projectLambdaDelete.handler;
+
+const projectLambdaGet = require('./src/project/Project_Lambda_Get.res.js');
+exports.projectLambdaGet = projectLambdaGet.handler;
+
+const projectLambdaGetAll = require('./src/project/Project_Lambda_GetAll.res.js');
+exports.projectLambdaGetAll = projectLambdaGetAll.handler;
+
+const projectLambdaSave = require('./src/project/Project_Lambda_Save.res.js');
+exports.projectLambdaSave = projectLambdaSave.handler;
+
+const sessionLambdaGet = require('./src/session/Session_Lambda_Get.res.js');
+exports.sessionLambdaGet = sessionLambdaGet.handler;
+
+const sessionLambdaSave = require('./src/session/Session_Lambda_Save.res.js');
+exports.sessionLambdaSave = sessionLambdaSave.handler;
