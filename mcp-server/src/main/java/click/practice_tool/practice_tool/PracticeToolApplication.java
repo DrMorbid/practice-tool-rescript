0a5f4a1b861e27e068588fd2665738d2e9258538
Filip Kittnar <drmorbid@seznam.cz>
2025-10-26 18:40:27 +0100

    Add MCP Server project

---
+++ mcp-server/src/main/java/click/practice_tool/practice_tool/PracticeToolApplication.java
@@ -0,0 +1,13 @@
+package click.practice_tool.practice_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeToolApplication.class, args);
+	}
+
+}
---
+++ mcp-server/src/test/java/click/practice_tool/practice_tool/PracticeToolApplicationTests.java
@@ -0,0 +1,13 @@
+package click.practice_tool.practice_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
